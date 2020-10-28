65be159dac4d1ddd3e91ee0d1725f196481e711c
Andres Wellmann <wellmann39@gmail.com>
2020-10-28 13:41:39 -0600

    Login con google creado y mantener sesion iniciada

--- app/src/main/java/com/example/cronometro/HomeActivity.kt
+++ app/src/main/java/com/example/cronometro/HomeActivity.kt
@@ -1,17 +1,24 @@
 package com.example.cronometro
 
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
+
 enum class ProviderType {
-    BASIC
+    BASIC,
+    GOOGLE
 }
 
 class HomeActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -21,13 +28,24 @@ class HomeActivity : AppCompatActivity() {
             val intent:Intent = Intent(this, ActivityCronometro::class.java)
             startActivity(intent)
         }
-        //Ignorar comentario
+
         //Setup
         val bundle = intent.extras
         val email =  bundle?.getString("email")
         val provider = bundle?.getString("provider")
         setup(email ?: "", provider ?: "")
+
+        //Guardado de datos
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
     }
+
+
+
+
 
     private fun setup(email: String, provider: String) {
 
@@ -36,6 +54,13 @@ class HomeActivity : AppCompatActivity() {
         providerTextView.text = provider
 
         logOutButton.setOnClickListener {
+
+            //Borrado de datos
+
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
